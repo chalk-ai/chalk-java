cb0c42710c95df418debbd0ffdc20898f7aa1642
Jin Hang Ng <jinhang@chalk.ai>
2023-09-05 12:00:21 -0700

    cleanup

--- src/main/java/ai/chalk/internal/config/models/ProjectToken.java
+++ src/main/java/ai/chalk/internal/config/models/ProjectToken.java
@@ -1,7 +1,5 @@
 package ai.chalk.internal.config.models;
 
-import ai.chalk.internal.config.models.DeserializableLocalDateTime;
-import ai.chalk.internal.config.models.JWT;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,19 +15,4 @@ public class ProjectToken {
     private String apiServer;
     private String activeEnvironment;
     private JWT jwt;
-
-
-//    public String getName() { return name; }
-//
-//    public String getClientId() { return clientId; }
-//
-//    public String getClientSecret() { return clientSecret; }
-//
-//    public DeserializableLocalDateTime getValidUntil() { return validUntil; }
-//
-//    public String getApiServer() { return apiServer; }
-//
-//    public String getActiveEnvironment() { return activeEnvironment; }
-//
-//    public JWT getJwt() { return jwt; }
 }
--- src/main/java/ai/chalk/internal/config/models/ProjectTokens.java
+++ src/main/java/ai/chalk/internal/config/models/ProjectTokens.java
@@ -1,12 +1,12 @@
 package ai.chalk.internal.config.models;
 
+import lombok.Data;
+
 import java.util.HashMap;
 
+@Data
 public class ProjectTokens {
 
     private HashMap<String, ProjectToken> tokens;
 
-    // getters and setters
-    public HashMap<String, ProjectToken> getTokens() { return tokens; }
-    public void setTokens(HashMap<String, ProjectToken> tokens) { this.tokens = tokens; }
 }
