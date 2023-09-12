4206be2874f738f4fc976a3adcbb7be49b71c671
Jin Hang Ng <jinhang@chalk.ai>
2023-09-11 18:22:52 -0700

    manual fermet

--- src/main/java/ai/chalk/models/OnlineQueryParamsComplete.java
+++ src/main/java/ai/chalk/models/OnlineQueryParamsComplete.java
@@ -5,7 +5,20 @@ import java.util.List;
 import java.util.Map;
 
 public class OnlineQueryParamsComplete extends OnlineQueryParams {
-    public OnlineQueryParamsComplete(Map<String, Object> inputs, List<String> outputs, Map<String, Duration> staleness, Map<String, String> meta, List<String> tags, boolean includeMeta, boolean includeMetrics, String environmentId, String previewDeploymentId, String queryName, String correlationId, String branch) {
+    public OnlineQueryParamsComplete(
+            Map<String, Object> inputs,
+            List<String> outputs,
+            Map<String, Duration> staleness,
+            Map<String, String> meta,
+            List<String> tags,
+            boolean includeMeta,
+            boolean includeMetrics,
+            String environmentId,
+            String previewDeploymentId,
+            String queryName,
+            String correlationId,
+            String branch,
+    ) {
         super(inputs, outputs, staleness, meta, tags, includeMeta, includeMetrics, environmentId, previewDeploymentId, queryName, correlationId, branch);
     }
 }
