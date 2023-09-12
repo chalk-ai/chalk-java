cbc6e3c3493f51d173ff9b6a2eb39317a1396693
Jin Hang Ng <jinhang@chalk.ai>
2023-09-11 18:54:11 -0700

    fix test

--- src/test/java/ai/chalk/client/TestChalkClient.java
+++ src/test/java/ai/chalk/client/TestChalkClient.java
@@ -5,32 +5,19 @@ import org.junit.jupiter.api.Test;
 
 public class TestChalkClient {
     @Test
-    public void test() {
+    public void test() throws Exception {
         ChalkClient client = null;
         int[] userIds = new int[] {1};
-        try {
-            client = ChalkClient.create();
+        client = ChalkClient.create();
+        client.printConfig();
 
-            client = ChalkClient.builder()
-                    .withApiServer("https://api.chalk.ai")
-                    .withEnvironmentId("tmnmc8beyujew")
-                    .withClientId("98wrasfg7dge7wdasg709")
-                    .withClientSecret("h23lkj4h23lkj4z9s78fg908as7fkjh324klj23")
-                    .build();
-
-            client.printConfig();
-
-            var result = client.onlineQuery(
-                OnlineQueryParams.builder()
-                    .withInput("user.id", userIds)
-                    .withOutput("user.socure_score")
-                    .build()
-            );
-            assert result.getScalarsTable().getRowCount() == userIds.length;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
+        var result = client.onlineQuery(
+            OnlineQueryParams.builder()
+                .withInput("user.id", userIds)
+                .withOutput("user.socure_score")
+                .build()
+        );
+        assert result.getScalarsTable().getRowCount() == userIds.length;
     }
 }
 
