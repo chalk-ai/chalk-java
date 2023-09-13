b72677b447d18c34293cbac6e70e6328d6c0bc1f
Jin Hang Ng <jinhang@chalk.ai>
2023-09-12 18:43:54 -0700

    cleanup

--- src/test/java/chalk/client/TestChalkClient.java
+++ src/test/java/chalk/client/TestChalkClient.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.Test;
 public class TestChalkClient {
     @Test
     public void test() throws Exception {
-        ChalkClient client = ChalkClient.builder().withClientSecret("secret-a522c4fa81838666fed8d5a8574bbf8f384129806718b2aae59ec7b468068496").withApiServer("http://127.0.0.1:8000").withEnvironmentId("ftdev").withClientId("client-b9c4c78b8226b480be339f7ec8b5dfb4").build();
+        ChalkClient client = ChalkClient.create();
         client.printConfig();
-        int[] userIds = new int[1_000_000];
+        int[] userIds = new int[10];
         for (int i = 0; i < userIds.length; i++) {
             userIds[i] = i;
         }
