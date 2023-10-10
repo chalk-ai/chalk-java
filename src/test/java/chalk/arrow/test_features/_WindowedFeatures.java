7c244486f2f2c0bd7f5a3fd48a65d81f0811c03a
Jin Hang Ng <jinhang@chalk.ai>
2023-10-09 18:58:42 -0700

    revert bad change

--- src/test/java/chalk/arrow/test_features/_WindowedFeatures.java
+++ src/test/java/chalk/arrow/test_features/_WindowedFeatures.java
@@ -7,6 +7,4 @@ import chalk.features.WindowedFeaturesClass;
 public class _WindowedFeatures extends WindowedFeaturesClass {
     public Feature<Double> _1d;
     public Feature<Double> _601s;
-
-    public Feature<Double> window_1m;
 }
