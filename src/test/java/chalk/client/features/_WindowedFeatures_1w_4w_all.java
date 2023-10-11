ab89c2cd1eb203b00723ca2575f7bde38fc71bb1
Jin Hang Ng <jinhang@chalk.ai>
2023-10-11 10:52:32 -0700

    more nit renames

--- src/test/java/chalk/client/features/_WindowedFeatures_1w_4w_all.java
+++ src/test/java/chalk/client/features/_WindowedFeatures_1w_4w_all.java
@@ -3,7 +3,7 @@ package chalk.client.features;
 import chalk.features.Feature;
 import chalk.features.WindowedFeaturesClass;
 
-public class _WindowedFeatures extends WindowedFeaturesClass {
+public class _WindowedFeatures_1w_4w_all extends WindowedFeaturesClass {
     public Feature<Double> bucket_1w;
     public Feature<Double> bucket_4w;
     public Feature<Double> bucket_all;
