3382de67a682dd279a52244b52d2566f8a53cbd8
Jin Hang Ng <jinhang@chalk.ai>
2023-11-22 17:51:50 -0800

    prep e2e test

---
+++ src/test/java/ai/chalk/client/e2e/FraudTemplateFeatures.java
@@ -0,0 +1,14 @@
+package ai.chalk.client.e2e;
+
+import ai.chalk.client.e2e.User;
+import ai.chalk.internal.codegen.Initializer;
+
+public class FraudTemplateFeatures {
+    public static User user;
+
+    public static Exception initException = Initializer.initFeatures(FraudTemplateFeatures.class);
+
+    public static Exception getInitException() {
+        return FraudTemplateFeatures.initException;
+    }
+}
---
+++ src/test/java/ai/chalk/client/e2e/User.java
@@ -0,0 +1,9 @@
+package ai.chalk.client.e2e;
+
+import ai.chalk.features.Feature;
+import ai.chalk.features.FeaturesClass;
+
+public class User extends FeaturesClass {
+    public Feature<String> id;
+    public Feature<Double> socure_score;
+}
