28fc955555703595346db72f49de67bdb7206c11
Elliot Marx <elliot@chalk.ai>
2024-04-24 09:37:48 -0700

    Minor cleanup of config stuff

--- src/main/java/ai/chalk/client/ChalkClientImpl.java
+++ src/main/java/ai/chalk/client/ChalkClientImpl.java
@@ -1,26 +1,25 @@
 package ai.chalk.client;
 
 
-import ai.chalk.internal.request.RequestHandler;
-import ai.chalk.models.OnlineQueryParamsComplete;
-import ai.chalk.models.OnlineQueryResult;
 import ai.chalk.exceptions.ChalkException;
 import ai.chalk.exceptions.ClientException;
 import ai.chalk.internal.bytes.BytesProducer;
 import ai.chalk.internal.config.Loader;
 import ai.chalk.internal.config.models.ProjectToken;
 import ai.chalk.internal.config.models.SourcedConfig;
-import ai.chalk.internal.request.models.SendRequestParams;
+import ai.chalk.internal.request.RequestHandler;
 import ai.chalk.internal.request.models.OnlineQueryBulkResponse;
-import lombok.Data;
+import ai.chalk.internal.request.models.SendRequestParams;
+import ai.chalk.models.OnlineQueryParamsComplete;
+import ai.chalk.models.OnlineQueryResult;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class ChalkClientImpl implements ChalkClient {
-    private SourcedConfig apiServer;
-    private SourcedConfig clientId;
-    private SourcedConfig environmentId;
+    private final SourcedConfig apiServer;
+    private final SourcedConfig clientId;
+    private final SourcedConfig environmentId;
     private SourcedConfig initialEnvironment;
     private SourcedConfig clientSecret;
     private String branch;
@@ -74,7 +73,9 @@ public class ChalkClientImpl implements ChalkClient {
         return this.handler.sendRequest(request).toResult();
     }
 
-    public record ResolvedConfig(SourcedConfig apiServer, SourcedConfig clientId, SourcedConfig clientSecret, SourcedConfig environmentId) {}
+    public record ResolvedConfig(SourcedConfig apiServer, SourcedConfig clientId, SourcedConfig clientSecret,
+                                 SourcedConfig environmentId) {
+    }
 
     private ResolvedConfig resolveConfig(BuilderImpl builder) throws ClientException {
         ProjectToken chalkYamlConfig = new ProjectToken();
@@ -87,50 +88,56 @@ public class ChalkClientImpl implements ChalkClient {
             // TODO: Add some logging here
         }
 
-        SourcedConfig apiServerBuilder = SourcedConfig.fromBuilder(builder.getApiServer());
-        SourcedConfig clientIdBuilder = SourcedConfig.fromBuilder(builder.getClientId());
-        SourcedConfig clientSecretBuilder = SourcedConfig.fromBuilder(builder.getClientSecret());
-        SourcedConfig environmentIdBuilder = SourcedConfig.fromBuilder(builder.getEnvironmentId());
+        ResolvedConfig config = new ResolvedConfig(
+                SourcedConfig.firstNonEmpty(
+                        SourcedConfig.fromBuilder(builder.getApiServer()),
+                        SourcedConfig.fromEnvVar(ConfigEnvVars.apiServerKey),
+                        SourcedConfig.fromConfigFile(chalkYamlConfig.getApiServer()),
+                        new SourcedConfig("default", "https://api.chalk.ai")
+                ),
+                SourcedConfig.firstNonEmpty(
+                        SourcedConfig.fromBuilder(builder.getClientId()),
+                        SourcedConfig.fromEnvVar(ConfigEnvVars.clientIdKey),
+                        SourcedConfig.fromConfigFile(chalkYamlConfig.getClientId())
+                ),
+                SourcedConfig.firstNonEmpty(
+                        SourcedConfig.fromBuilder(builder.getClientSecret()),
+                        SourcedConfig.fromEnvVar(ConfigEnvVars.clientSecretKey),
+                        SourcedConfig.fromConfigFile(chalkYamlConfig.getClientSecret())
+                ),
+                SourcedConfig.firstNonEmpty(
+                        SourcedConfig.fromBuilder(builder.getEnvironmentId()),
+                        SourcedConfig.fromEnvVar(ConfigEnvVars.environmentIdKey),
+                        SourcedConfig.fromConfigFile(chalkYamlConfig.getActiveEnvironment())
+                )
+        );
 
-        SourcedConfig apiServerEnvVar = SourcedConfig.fromEnvVar(ConfigEnvVars.apiServerKey);
-        SourcedConfig clientIdEnvVar = SourcedConfig.fromEnvVar(ConfigEnvVars.clientIdKey);
-        SourcedConfig clientSecretEnvVar = SourcedConfig.fromEnvVar(ConfigEnvVars.clientSecretKey);
-        SourcedConfig environmentIdEnvVar = SourcedConfig.fromEnvVar(ConfigEnvVars.environmentIdKey);
-
-        SourcedConfig apiServerChalkYaml = SourcedConfig.fromConfigFile(chalkYamlConfig.getApiServer());
-        SourcedConfig clientIdChalkYaml = SourcedConfig.fromConfigFile(chalkYamlConfig.getClientId());
-        SourcedConfig clientSecretChalkYaml = SourcedConfig.fromConfigFile(chalkYamlConfig.getClientSecret());
-        SourcedConfig environmentIdChalkYaml = SourcedConfig.fromConfigFile(chalkYamlConfig.getActiveEnvironment());
-
-        SourcedConfig apiServer = SourcedConfig.firstNonEmpty(apiServerBuilder, apiServerEnvVar, apiServerChalkYaml, new SourcedConfig("default", "https://api.chalk.ai"));
-        SourcedConfig clientId = SourcedConfig.firstNonEmpty(clientIdBuilder, clientIdEnvVar, clientIdChalkYaml);
-        SourcedConfig clientSecret = SourcedConfig.firstNonEmpty(clientSecretBuilder, clientSecretEnvVar, clientSecretChalkYaml);
-        SourcedConfig environmentId = SourcedConfig.firstNonEmpty(environmentIdBuilder, environmentIdEnvVar, environmentIdChalkYaml);
-
-
-        if (clientId.getValue().isEmpty() || clientSecret.getValue().isEmpty() || apiServer.getValue().isEmpty() || environmentId.getValue().isEmpty()) {
+        if (config.clientId.getValue().isEmpty() ||
+                config.clientSecret.getValue().isEmpty() ||
+                config.apiServer.getValue().isEmpty() ||
+                config.environmentId.getValue().isEmpty()) {
             System.err.println(this.getConfigStr());
             throw new ClientException("Chalk's config variables are not set correctly. See error log for more details.");
         }
 
-        return new ResolvedConfig(apiServer, clientId, clientSecret, environmentId);
+        return config;
     }
 
     private String getConfigStr() {
         String preTable = """
 
 
-ChalkClient's config variables and the source of these variables are displayed in the following table.
-""";
+                ChalkClient's config variables and the source of these variables are displayed in the following table.
+                """;
         String postTable = """
 
-For each variable, we take the first non-empty value, in order, from the following sources:
-  1. The value passed to ChalkClient's Builder
-  2. The value of the config's corresponding environment variable (see the class `ai.chalk.client.ConfigEnvVars`)
-  3. The value in the project root's 'chalk.yaml' or 'chalk.yml' file
-  
-  
-""";
+                For each variable, we take the first non-empty value, in order, from the following sources:
+                  1. The value passed to ChalkClient's Builder
+                  2. The value of the config's corresponding environment variable (see the class `ai.chalk.client.ConfigEnvVars`)
+                  3. The value in the project root's 'chalk.yaml' or 'chalk.yml' file
+                  4. A default value (if applicable)
+                  
+                """;
 
         Map<String, SourcedConfig> configMap = new HashMap<>();
         configMap.put("Api Server", this.apiServer);
