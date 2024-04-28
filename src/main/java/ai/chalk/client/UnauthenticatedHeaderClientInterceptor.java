19dd9ad29e4c278614e1f4aef621c48afb7cc2b8
Elliot Marx <elliot@chalk.ai>
2024-04-28 09:34:50 -0700

    Unauthenticated rpc (#84)

--- src/main/java/ai/chalk/client/UnauthenticatedHeaderClientInterceptor.java
+++ src/main/java/ai/chalk/client/UnauthenticatedHeaderClientInterceptor.java
@@ -3,36 +3,38 @@ package ai.chalk.client;
 import io.grpc.*;
 import io.grpc.ForwardingClientCall.SimpleForwardingClientCall;
 import io.grpc.stub.MetadataUtils;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
-import java.util.logging.Logger;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A client interceptor that adds the server type and environment ID to the headers of all outgoing
  */
 public class UnauthenticatedHeaderClientInterceptor implements ClientInterceptor {
-    private static final Logger logger = Logger.getLogger(UnauthenticatedHeaderClientInterceptor.class.getName());
-    private final String serverType;
-    private final String environmentId;
-
-    public UnauthenticatedHeaderClientInterceptor(
-            String environmentId,
-            String serverType
-    ) {
-        this.environmentId = environmentId;
-        this.serverType = serverType;
+    public UnauthenticatedHeaderClientInterceptor(@NonNull Map<String, String> additionalHeaders) {
+        this.allHeaders = new HashMap<>(Map.of(
+                GrpcHeaders.SERVER_TYPE_KEY,
+                ServerType.SERVER.headerName()
+        ));
+        for (Map.Entry<String, String> entry : additionalHeaders.entrySet()) {
+            this.allHeaders.put(Metadata.Key.of(entry.getKey(), Metadata.ASCII_STRING_MARSHALLER), entry.getValue());
+        }
     }
+
+    private final @NonNull Map<Metadata.Key<String>, String> allHeaders;
 
     @Override
     public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(
             MethodDescriptor<ReqT, RespT> method,
             CallOptions callOptions, Channel next
     ) {
-        return new SimpleForwardingClientCall<ReqT, RespT>(next.newCall(method, callOptions)) {
-
+        return new SimpleForwardingClientCall<>(next.newCall(method, callOptions)) {
             @Override
             public void start(Listener<RespT> responseListener, Metadata headers) {
-                headers.put(GrpcHeaders.SERVER_TYPE_KEY, serverType);
-                headers.put(GrpcHeaders.ENVIRONMENT_ID_KEY, environmentId);
+                for (Map.Entry<Metadata.Key<String>, String> entry : allHeaders.entrySet()) {
+                    headers.put(entry.getKey(), entry.getValue());
+                }
 
                 MetadataUtils.newAttachHeadersInterceptor(headers).interceptCall(
                         method,
