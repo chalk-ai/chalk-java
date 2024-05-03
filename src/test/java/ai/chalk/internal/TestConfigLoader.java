a80831e68f52328b18b855ca194a0cd3e39ab90b
rooftoofwoof <jinhang@chalk.ai>
2024-05-02 17:19:23 -0700

    test jwt

--- src/test/java/ai/chalk/internal/TestConfigLoader.java
+++ src/test/java/ai/chalk/internal/TestConfigLoader.java
@@ -2,10 +2,12 @@ package ai.chalk.internal;
 
 import ai.chalk.internal.config.Loader;
 import ai.chalk.internal.config.models.ProjectTokens;
+import ai.chalk.internal.request.RequestHandler;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -84,6 +86,28 @@ public class TestConfigLoader {
 
         var zonedToken = getProjectToken(config, tempFile.toString(), "zoned");
         assert zonedToken.getValidUntil().equals(ZonedDateTime.of(LocalDateTime.of(2025, 3, 6, 10, 38, 4, 372_277_029), ZoneId.of("UTC")));
+    }
+
+    @Test
+    public void TestParseJWT() throws Exception {
+        String content = """
+            tokens:
+              default:
+                name: bogus
+                clientId: bogus
+                clientSecret: bogus
+                validUntil: 2025-03-06 10:38:04.372277029 +0000 UTC
+                apiServer: bogus
+                activeEnvironment: bogus
+                jwt:
+                    value: bogus
+                    validUntil: 2024-04-30T05:16:28.750539Z
+            """;
+
+        var tempFile = writeTempYAMLFile(content);
+        ProjectTokens config = Loader.loadAllTokens(tempFile);
+        var token = getProjectToken(config, tempFile.toString(), "/project_does_not_exist_use_default");
+        assert token.getJwt().getValidUntil().equals(ZonedDateTime.of(LocalDateTime.of(2024, 4, 30, 5, 16, 28, 750_539_000), ZoneId.of("UTC")));
     }
 }
 
