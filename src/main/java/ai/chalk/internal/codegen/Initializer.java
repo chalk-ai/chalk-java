3dc35beae523a4dcdd1db3671b51c5cbbcfb2fec
rooftoofwoof <jinhang@chalk.ai>
2024-05-03 17:10:07 -0700

    handle versioned features

--- src/main/java/ai/chalk/internal/codegen/Initializer.java
+++ src/main/java/ai/chalk/internal/codegen/Initializer.java
@@ -1,16 +1,10 @@
 package ai.chalk.internal.codegen;
 
-import ai.chalk.features.Feature;
-import ai.chalk.features.FeaturesBase;
-import ai.chalk.features.StructFeaturesClass;
-import ai.chalk.features.WindowedFeaturesClass;
+import ai.chalk.features.*;
 import ai.chalk.internal.Utils;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Initializer {
@@ -31,12 +25,12 @@ public class Initializer {
         return null;
     }
 
-    public static Map<String, Feature<?>> initResult(FeaturesBase fc) throws Exception {
+    public static Map<String, List<Feature<?>>> initResult(FeaturesBase fc) throws Exception {
         Field[] myFields = fc.getClass().getDeclaredFields();
         Field[] parentFields = fc.getClass().getSuperclass().getDeclaredFields();
         Field[] fields = Stream.concat(Arrays.stream(myFields), Arrays.stream(parentFields)).toArray(Field[]::new);
 
-        Map<String, Feature<?>> featureMap = new java.util.HashMap<>();
+        Map<String, List<Feature<?>>> featureMap = new java.util.HashMap<>();
         var rootFeatureFqn = Utils.toSnakeCase(fc.getClass().getSimpleName());
         for (Field field : fields) {
             var childFqn = rootFeatureFqn + "." + field.getName();
@@ -46,7 +40,7 @@ public class Initializer {
         return featureMap;
     }
 
-    public static Object init(Field f, String fqn, Map<String, Feature<?>> featureMap, Set<Class<?>> seenClassesInChain) throws Exception {
+    public static Object init(Field f, String fqn, Map<String, List<Feature<?>>> featureMap, Set<Class<?>> seenClassesInChain) throws Exception {
         if (FeaturesBase.class.isAssignableFrom(f.getType())) {
             if (seenClassesInChain.contains(f.getType())) {
                 // We simply stop initing
@@ -81,9 +75,35 @@ public class Initializer {
         } else if (f.getType() == Feature.class) {
             // BASE CASE
             Feature<?> feature = (Feature<?>) f.getType().getConstructor().newInstance();
+            if (f.isAnnotationPresent(Versioned.class)) {
+                Versioned versionInfo = f.getAnnotation(Versioned.class);
+                if (versionInfo.defaultVersion() == 0) {
+                    // Is not base version feature, so we need to strip the `_vN` suffix by splitting on '_'
+                    String[] parts = fqn.split("_");
+                    // Parse the digits from the last part of the FQN, which looks something like `v1`
+                    String versionStr = parts[parts.length - 1].substring(1);
+                    String baseFqn = String.join("_", Arrays.copyOf(parts, parts.length - 1));
+                    if (versionStr.equals("1")) {
+                        // If the version is 1, we don't need to append it to the FQN
+                        fqn = baseFqn;
+                    } else {
+                        fqn = baseFqn + "@" + versionStr;
+                    }
+                } else if (versionInfo.defaultVersion() > 1) {
+                    // Is base version feature, so we need to append the default version to the FQN
+                    fqn += "@" + versionInfo.defaultVersion();
+                }
+            }
             feature.setFqn(fqn);
             if (featureMap != null) {
-                featureMap.put(fqn, feature);
+                if (featureMap.containsKey(fqn)) {
+                    // Only versioned features should have multiple features with the same FQN
+                    // i.e. `Features.user.versioned_feature` and `Features.user.versioned_feature_v2`
+                    //       have the same FQN `user.versioned_feature@2` if the default version is 2.
+                    featureMap.get(fqn).add(feature);
+                } else {
+                    featureMap.put(fqn, new ArrayList<>(List.of(feature)));
+                }
             }
             return feature;
         } else {
