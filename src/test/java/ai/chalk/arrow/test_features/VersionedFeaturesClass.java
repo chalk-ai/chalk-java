d8fc86ead48037da17ad223cdd37fdf493aa7839
rooftoofwoof <jinhang@chalk.ai>
2024-05-30 11:50:43 -0700

    fixme

--- src/test/java/ai/chalk/arrow/test_features/VersionedFeaturesClass.java
+++ src/test/java/ai/chalk/arrow/test_features/VersionedFeaturesClass.java
@@ -7,6 +7,8 @@ import ai.chalk.features.Versioned;
 public class VersionedFeaturesClass extends FeaturesClass {
     @Versioned(defaultVersion = 2)
     public Feature<String> grade;
+
+    // FIXME: This needs to be pascal case?
     @Versioned
     public Feature<String> grade_v1;
     @Versioned
