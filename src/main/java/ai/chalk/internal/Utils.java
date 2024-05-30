e1fe6ecf0826e3a9b6677ebae234a5f5f643b2cf
rooftoofwoof <jinhang@chalk.ai>
2024-05-29 21:09:31 -0700

    nevermind

--- src/main/java/ai/chalk/internal/Utils.java
+++ src/main/java/ai/chalk/internal/Utils.java
@@ -80,7 +80,7 @@ public class Utils {
     }
 
 
-    public static Field getFieldFromFqn(Class<?> clazz, String fqn) {
+    public static Field getFieldFromFqn(Class<?> clazz, String fqn) throws Exception {
         String featureName = Utils.getDotDelimitedLastSection(fqn);
         for (Field field : clazz.getDeclaredFields()) {
             if (Utils.getResolvedName(field).equals(featureName)) {
