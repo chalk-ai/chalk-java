07d750d6fcb6cb8804ff401c307bffd0f67d5a57
rooftoofwoof <jinhang@chalk.ai>
2024-06-18 12:26:29 -0700

    update protos from grpc 1.63 to 1.64

--- src/main/java/ai/chalk/protos/chalk/engine/v1/QueryServiceGrpc.java
+++ src/main/java/ai/chalk/protos/chalk/engine/v1/QueryServiceGrpc.java
@@ -5,7 +5,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 /**
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.63.0)",
+    value = "by gRPC proto compiler (version 1.64.0)",
     comments = "Source: chalk/engine/v1/query_server.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class QueryServiceGrpc {
