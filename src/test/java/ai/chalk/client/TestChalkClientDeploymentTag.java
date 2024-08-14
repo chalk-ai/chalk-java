bac511f81fa89db0a4e7ccd0ba6caf363b9f71be
Samuel Mignot <sjmignot@gmail.com>
2024-08-14 15:47:09 -0700

    update deployment tag test name

--- src/test/java/ai/chalk/client/TestChalkClientDeploymentTag.java
+++ src/test/java/ai/chalk/client/TestChalkClientDeploymentTag.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 
-public class TestChalkClient {
+public class TestChalkClientDeploymentTag {
 
     private static ChalkClient client;
 
