5d54ffd7bf00b670f054f2b1f3df42e7b33f75c3
rooftoofwoof <jinhang@chalk.ai>
2024-09-04 13:00:49 -0700

    skip all __chalk__ prefixed columns

--- src/main/java/ai/chalk/internal/Constants.java
+++ src/main/java/ai/chalk/internal/Constants.java
@@ -3,5 +3,5 @@ package ai.chalk.internal;
 public class Constants {
     public static String tsFeatureFqn = "__ts__";
     public static String indexFqn = "__index__";
-    public static String metadataPrefix = "__chalk__.__metadata__.";
+    public static String chalkDunderPrefix = "__chalk__.";
 }
