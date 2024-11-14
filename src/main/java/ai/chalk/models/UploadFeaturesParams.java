3d44a49f80e5501967b0d1534123a5821ffdad6f
rooftoofwoof <jinhang@chalk.ai>
2024-11-13 18:26:48 -0800

    custom builder

--- src/main/java/ai/chalk/models/UploadFeaturesParams.java
+++ src/main/java/ai/chalk/models/UploadFeaturesParams.java
@@ -1,16 +1,66 @@
 package ai.chalk.models;
 
-import lombok.Builder;
+import ai.chalk.features.Feature;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Getter
-@Builder(setterPrefix = "with")
+
+@AllArgsConstructor @Getter
 public class UploadFeaturesParams {
     private final Map<String, List<?>> inputs;
     private final String environmentId;
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Builder {
+        protected Map<String, List<?>> inputs;
+        protected String environmentId;
+
+        public UploadFeaturesParams build() {
+            return new UploadFeaturesParams(this.inputs, this.environmentId);
+        }
+
+        private void initInputs() {
+            if (this.inputs == null) {
+                this.inputs = new HashMap<>();
+            }
+        }
+
+        private Builder _withInput(String fqn, List<?> value) {
+            this.initInputs();
+            this.inputs.put(fqn, value);
+            return this;
+        }
+
+        public Builder withInputs(Map<String, List<?>> inputs) {
+            this.initInputs();
+            this.inputs.putAll(inputs);
+            return this;
+        }
+
+        public <T> Builder withFeatureInputs(Map<Feature<T>, List<T>> inputs) {
+            this.initInputs();
+            inputs.forEach((feature, value) -> this.inputs.put(feature.getFqn(), value));
+            return this;
+        }
+
+        public Builder withInput(Feature<?> feature, List<?> value) {
+            return this._withInput(feature.getFqn(), value);
+        }
+
+        public Builder withInput(String feature, List<?> value) {
+            return this._withInput(feature, value);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
 }
 
 
