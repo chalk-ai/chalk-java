1e523da51fb915658f47321115450fbfb51d3d3a
rooftoofwoof <jinhang@chalk.ai>
2025-01-20 15:27:15 -0800

    fix memo test

--- src/test/java/ai/chalk/internal/TestNamespaceMemo.java
+++ src/test/java/ai/chalk/internal/TestNamespaceMemo.java
@@ -11,31 +11,101 @@ import java.util.HashSet;
 import java.util.List;
 
 public class TestNamespaceMemo {
+    private void assertIsScalarFeature(FieldMeta meta) {
+        assert meta.isFeature();
+        assert !meta.isList();
+        assert !meta.isStruct();
+        assert !meta.isWindowed();
+        assert !meta.isFeaturesBase();
+        assert !meta.isFeaturesClass();
+    }
+
+    private void assertIsListFeature(FieldMeta meta) {
+        assert meta.isFeature();
+        assert meta.isList();
+        assert !meta.isStruct();
+        assert !meta.isWindowed();
+        assert !meta.isFeaturesBase();
+        assert !meta.isFeaturesClass();
+    }
+
+    private void assertIsStructFeature(FieldMeta meta) {
+        assert !meta.isFeature();
+        assert !meta.isList();
+        assert meta.isStruct();
+        assert !meta.isWindowed();
+        assert meta.isFeaturesBase();
+        assert !meta.isFeaturesClass();
+    }
+
+    private void assertIsWindowedFeature(FieldMeta meta) {
+        assert !meta.isFeature();
+        assert !meta.isList();
+        assert !meta.isStruct();
+        assert meta.isWindowed();
+        assert meta.isFeaturesBase();
+        assert !meta.isFeaturesClass();
+    }
+
+
     @Test
     public void TestBuildNamespaceMemo() throws Exception {
         var memo = new HashMap<Class<?>, NamespaceMemoItem>();
         Initializer.buildNamespaceMemo(User.class, memo, new HashSet<>());
+
+        // User
         assert memo.containsKey(User.class);
         var userMemo = memo.get(User.class);
-        assert userMemo.resolvedNameToFieldMeta.get("id").equals(List.of(0));
-        assert userMemo.resolvedNameToFieldMeta.get("name").equals(List.of(1));
-        assert userMemo.resolvedNameToFieldMeta.get("transactions").equals(List.of(2));
-        // FIXME
-        // assert userMemo.isFieldFeaturesBaseSubclass.equals(List.of(false, false, false, false));
+        FieldMeta idMeta = userMemo.resolvedNameToFieldMeta.get("id").get(0);
+        assert idMeta.field().equals(User.class.getField("id"));
+        assertIsScalarFeature(idMeta);
 
+        FieldMeta nameMeta = userMemo.resolvedNameToFieldMeta.get("name").get(0);
+        assert nameMeta.field().equals(User.class.getField("name"));
+        assertIsScalarFeature(nameMeta);
+
+        FieldMeta transactionsMeta = userMemo.resolvedNameToFieldMeta.get("transactions").get(0);
+        assert transactionsMeta.field().equals(User.class.getField("transactions"));
+        assertIsListFeature(transactionsMeta);
+        assert transactionsMeta.listUnderlyingClass().equals(Transaction.class);
+
+        // Transaction
         assert memo.containsKey(Transaction.class);
         var transactionMemo = memo.get(Transaction.class);
-        assert transactionMemo.resolvedNameToFieldMeta.get("id").equals(List.of(0));
-        assert transactionMemo.resolvedNameToFieldMeta.get("amount").equals(List.of(1));
-        assert transactionMemo.resolvedNameToFieldMeta.get("name@2").equals(List.of(3, 4));
-        assert transactionMemo.resolvedNameToFieldMeta.get("name@3").equals(List.of(5));
-        // FIXME
-        // assert transactionMemo.isFieldFeaturesBaseSubclass.equals(List.of(false, false, true, false, false, false, false));
+        FieldMeta txnId = transactionMemo.resolvedNameToFieldMeta.get("id").get(0);
+        assert txnId.field().equals(Transaction.class.getField("id"));
+        assertIsScalarFeature(txnId);
 
+        FieldMeta txnAmount = transactionMemo.resolvedNameToFieldMeta.get("amount").get(0);
+        assert txnAmount.field().equals(Transaction.class.getField("amount"));
+        assertIsScalarFeature(txnAmount);
+
+        FieldMeta txnName = transactionMemo.resolvedNameToFieldMeta.get("name@2").get(0);
+        assert txnName.field().equals(Transaction.class.getField("name"));
+        assertIsScalarFeature(txnName);
+
+        FieldMeta txnName2 = transactionMemo.resolvedNameToFieldMeta.get("name@2").get(1);
+        assert txnName2.field().equals(Transaction.class.getField("nameV2"));
+        assertIsScalarFeature(txnName2);
+
+        FieldMeta txnName3 = transactionMemo.resolvedNameToFieldMeta.get("name@3").get(0);
+        assert txnName3.field().equals(Transaction.class.getField("nameV3"));
+        assertIsScalarFeature(txnName3);
+
+        FieldMeta txnAvgAmount = transactionMemo.resolvedNameToFieldMeta.get("avg_amount").get(0);
+        assert txnAvgAmount.field().equals(Transaction.class.getField("avgAmount"));
+        assertIsWindowedFeature(txnAvgAmount);
+
+        // _WindowedFeatures_1d_30d
         assert memo.containsKey(_WindowedFeatures_1d_30d.class);
         var windowedMemo = memo.get(_WindowedFeatures_1d_30d.class);
-        assert windowedMemo.resolvedNameToFieldMeta.get("__86400__").equals(List.of(0));
-        assert windowedMemo.resolvedNameToFieldMeta.get("__2592000__").equals(List.of(1));
+        FieldMeta windowed1d = windowedMemo.resolvedNameToFieldMeta.get("__86400__").get(0);
+        assert windowed1d.field().equals(_WindowedFeatures_1d_30d.class.getField("bucket1d"));
+        assertIsScalarFeature(windowed1d);
+
+        FieldMeta windowed30d = windowedMemo.resolvedNameToFieldMeta.get("__2592000__").get(0);
+        assert windowed30d.field().equals(_WindowedFeatures_1d_30d.class.getField("bucket30d"));
+        assertIsScalarFeature(windowed30d);
     }
 }
 
--- src/test/java/ai/chalk/internal/test_features/Transaction.java
+++ src/test/java/ai/chalk/internal/test_features/Transaction.java
@@ -1,16 +1,17 @@
 package ai.chalk.internal.test_features;
 
+import ai.chalk.features.Feature;
 import ai.chalk.features.FeaturesClass;
 import ai.chalk.features.Versioned;
 
 public class Transaction extends FeaturesClass {
-    public String id;
-    public double amount;
+    public Feature<String> id;
+    public Feature<Double> amount;
     public _WindowedFeatures_1d_30d avgAmount;
     @Versioned( defaultVersion = 2 )
-    public String name;
+    public Feature<String> name;
     @Versioned
-    public String nameV2;
+    public Feature<String> nameV2;
     @Versioned
-    public String nameV3;
+    public Feature<String> nameV3;
 }
